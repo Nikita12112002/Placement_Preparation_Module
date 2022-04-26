e65b959fb3e27f50186b02ebd68f392dabd3c5b7
Nikita12112002 <guptaniki488@gmail.com>
2022-04-26 09:55:17 +0530

    commit

---
+++ Array/Array_part-I/Stock_buy_and_sell.cpp
@@ -0,0 +1,23 @@
+ //See picturized explanation of this from stock but and sell jpg
+  int maxProfit(vector<int>& prices) {
+        
+        int i,ind=0;
+        int max=0;
+        int min=prices[0];
+        for(i=1 ;i<prices.size();i++)
+        {
+            if(prices[i]<min)
+            {
+                min=prices[i];
+                ind=i;
+            }
+            else 
+            {
+                if(prices[i]-min>max)
+                {
+                    max=prices[i]-min;
+                }
+            }
+        }
+       return max;
+    }
