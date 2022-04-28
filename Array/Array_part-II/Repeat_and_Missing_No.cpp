059d947b6e7cf71167f838c880db440de4904894
Nikita12112002 <guptaniki488@gmail.com>
2022-04-28 20:16:45 +0530

    commit

---
+++ Array/Array_part-II/Repeat_and_Missing_No.cpp
@@ -0,0 +1,25 @@
+TC:O(n) SC:O(1);
+
+pair<int,int> missingAndRepeating(vector<int> &A, int n)
+{
+	// Write your code here 
+	pair<int,int>p;
+	
+	for(int i=0 ;i<n ;i++)
+    {
+        while(A[i]>0 && A[i]<=n && A[A[i]-1]!=A[i])
+        {
+            swap(A[i],A[A[i]-1]);
+        }
+    }
+        
+        for(int i=0 ;i<n ;i++)
+        {
+            if(A[i]!=i+1)
+            {
+                p.first=i+1;
+				p.second=A[i];
+			}
+        }
+        return p;
+}
