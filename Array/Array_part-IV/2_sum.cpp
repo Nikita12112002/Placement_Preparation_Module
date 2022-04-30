5ee15db338789259c4e59cc9c85e4478c512ff3f
Nikita12112002 <guptaniki488@gmail.com>
2022-04-30 22:55:20 +0530

    commi

---
+++ Array/Array_part-IV/2_sum.cpp
@@ -0,0 +1,13 @@
+// Problem Statement->
+// You are given an integer array 'ARR' of size 'N' and an integer 'S'. 
+// Your task is to return the list of all pairs of elements such that 
+// each sum of elements of each pair equals 'S'
+
+// input->
+// 5 0
+// 2 -3 3 3 -2
+
+// ouput->
+// -3 3
+// -3 3
+// -2 2
