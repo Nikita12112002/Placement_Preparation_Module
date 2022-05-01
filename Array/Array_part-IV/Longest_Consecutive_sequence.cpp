9e14f97a5a42a90cc1e174fe7d392efc7305b4ba
Nikita12112002 <guptaniki488@gmail.com>
2022-05-01 16:04:37 +0530

    commit

--- Array/Array_part-IV/Longest_Consecutive_sequence.cpp
+++ Array/Array_part-IV/Longest_Consecutive_sequence.cpp
@@ -1,3 +1,29 @@
+//Brute force
+
+int longestConsecutive(vector<int>& nums) 
+{
+	if(!size(nums))
+     return 0;
+	sort(begin(nums), end(nums));
+
+	int longest = 0, cur_longest = 1;
+
+	for(int i = 1; i < size(nums); i++) 
+		if(nums[i] == nums[i - 1]) 
+        continue;
+		else if(nums[i] == nums[i - 1] + 1) 
+        cur_longest++; // consecutive element - update current streak length
+		else 
+        {
+            longest = max(longest, cur_longest);
+           cur_longest = 1;  // reset current streak length
+        }
+	return max(longest, cur_longest);
+}
+
+
+//optimal
+
 int longestConsecutive(vector<int>& nums) {
         
         //TC:O(n)+O(n)+O(n)=O(3n)
