27e074c18c0f08a443e99e25c9b6f8d4952dfb45
Nikita12112002 <guptaniki488@gmail.com>
2022-05-01 06:59:40 +0530

    commit

--- Array/Array_part-IV/2_sum.cpp
+++ Array/Array_part-IV/2_sum.cpp
@@ -11,3 +11,34 @@
 // -3 3
 // -3 3
 // -2 2
+
+// This is to generate all pairs.
+
+vector<vector<int>> pairSum(vector<int> &arr, int sum)
+{
+   // Write your code here.
+	unordered_map<int, int> m;
+   vector<vector<int>>ans;
+	sort(arr.begin(),arr.end());
+    // Traverse through all elements
+    for (int i = 0; i < arr.size(); i++) {
+ 
+        // Search if a pair can be formed with
+        // arr[i].
+        int rem = sum - arr[i];
+        if (m.find(rem) != m.end())
+		{
+            int count = m[rem];
+            for (int j = 0; j < count; j++)
+			{
+				vector<int>res;
+                   res.push_back(rem); 
+                   res.push_back(arr[i]);
+				ans.push_back(res);
+			}
+        }
+        m[arr[i]]++;
+    }
+sort(ans.begin(),ans.end());
+ return ans;
+}
