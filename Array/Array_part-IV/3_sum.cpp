3975e11cfafce69d5c68eb7cb9736c6632a192a3
Nikita12112002 <guptaniki488@gmail.com>
2022-05-01 08:25:06 +0530

    commit

---
+++ Array/Array_part-IV/3_sum.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h> 
+vector<vector<int>> findTriplets(vector<int>nums, int n, int sum) {
+	// Write your code here
+	  
+         int i=0;
+        vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        
+        
+        for(i=0 ;i<n-2;i++)
+        {
+            if(i==0 || (i>0 && nums[i]!=nums[i-1]))
+            {
+                int low=i+1 ;
+                int high=n-1;
+                int target=sum-nums[i];
+                
+                while(low<high)
+                {
+                    if(nums[low]+nums[high]==target)
+                    {
+                        vector<int>res;
+                        res.push_back(nums[i]);
+                        res.push_back(nums[low]);
+                        res.push_back(nums[high]);
+                        ans.push_back(res);
+                        
+                        while(low<high && nums[low]==nums[low+1])
+                        low++;
+                    while(low<high && nums[high]==nums[high-1])
+                        high--;
+                    low++;
+                    high--;
+                    }
+                    
+                    if(nums[low]+nums[high]<target)
+                        low++;
+                    if(nums[low]+nums[high]>target)
+                        high--;
+                   
+                }
+            }
+
+        }
+        return ans;
+    
+}
