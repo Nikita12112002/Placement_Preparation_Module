5999bf04b4859f319798630af93e7cc3c9fcffdb
Nikita12112002 <guptaniki488@gmail.com>
2022-05-03 18:50:59 +0530

    commit

--- LinkedLists/LinkedLists-II/Check_if_a_LL_is_a_palindrome_or_not.cpp
+++ LinkedLists/LinkedLists-II/Check_if_a_LL_is_a_palindrome_or_not.cpp
@@ -4,3 +4,47 @@
 
 // Approach 2- optimal approach - TC:O(n) SC:O(1)
 
+bool isPalindrome(ListNode* head) {
+        
+        if(head==NULL || head->next==NULL)
+            return true;
+        
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        while(fast->next!=NULL && fast->next->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        
+        ListNode *prev=NULL,*newnode,*temp=slow->next;
+        
+        while(temp!=NULL)
+        {
+            newnode=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=newnode;
+        }
+        
+        slow->next=prev;
+        slow=slow->next;
+        
+        ListNode *dummy=head;
+        
+        while(slow!=NULL)
+        {
+            if(slow->val == head->val)
+            {
+            slow=slow->next;
+            head=head->next;
+            }
+            
+            else
+                return false;
+        }
+        
+        return true;
+    }
+    
