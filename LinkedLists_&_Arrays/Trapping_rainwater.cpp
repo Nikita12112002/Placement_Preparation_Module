755c9944233de0bedce6f09bf895117bf38397b7
Nikita12112002 <guptaniki488@gmail.com>
2022-05-04 13:22:40 +0530

    commit

--- LinkedLists_&_Arrays/Trapping_rainwater.cpp
+++ LinkedLists_&_Arrays/Trapping_rainwater.cpp
@@ -8,3 +8,35 @@
 // and then follow the formula min(left[i],right[i]-a[i]);
 
 //Approach 3- Two pointer
+ int trap(vector<int>& height) {
+        
+        int n=height.size();
+        int left=0 , right=n-1 ,leftmax=0 ,rightmax=0,res=0;
+        
+        while(left<=right)
+        {
+            if(height[left]<=height[right])
+            {
+                if(height[left]>=leftmax)
+                {
+                    leftmax=height[left];
+                }
+                else
+                {
+                    res=res+(leftmax-height[left]);
+                }
+                left++;
+            }
+            
+            else
+            {
+                if(height[right]>=rightmax)
+                    rightmax=height[right];
+                else
+                    res=res+(rightmax-height[right]);
+                    
+                    right--;
+            }
+        }
+        return res;
+    }
