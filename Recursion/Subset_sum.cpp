c15c6a061b4f9e56046f9e0ea6c43455a8e36446
Nikita12112002 <guptaniki488@gmail.com>
2022-05-20 21:07:55 +0530

    commit

---
+++ Recursion/Subset_sum.cpp
@@ -0,0 +1,6 @@
+//Brute force solution- power set method 
+//TC: O(n*2^n)
+
+// optimal sol - recursion TC:O(2^n) sc:(2^n)
+
+// Note- in subset questions always think of pick and non-pick approach.
