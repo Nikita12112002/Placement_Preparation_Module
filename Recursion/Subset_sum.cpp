a534930dd2294c3424f8ff7df4ee6650159ca36c
Nikita12112002 <guptaniki488@gmail.com>
2022-05-21 09:46:39 +0530

    commit

--- Recursion/Subset_sum.cpp
+++ Recursion/Subset_sum.cpp
@@ -4,3 +4,30 @@
 // optimal sol - recursion TC:O(2^n) sc:(2^n)
 
 // Note- in subset questions always think of pick and non-pick approach.
+
+class Solution
+{
+public:
+
+void solve(vector<int>&a , int n , int i , int sum ,vector<int>&ans)
+{
+    if(i==n)
+    {
+        ans.push_back(sum);
+        return;
+    }
+    else
+    {
+        solve(a,n,i+1,sum+a[i],ans);
+        solve(a,n,i+1,sum,ans);
+    }
+}
+
+    vector<int> subsetSums(vector<int> arr, int n)
+    {
+        // Write Your Code here
+        vector<int>ans;
+        solve(arr,n,0,0,ans);
+        return ans;
+    }
+};
