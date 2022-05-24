b288a8f4531107c18241f58e6a431976ba607182
Nikita12112002 <guptaniki488@gmail.com>
2022-05-24 21:30:03 +0530

    commit

--- Binary_Search/Upper_bound.cpp
+++ Binary_Search/Upper_bound.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
     int a[10]={1,2,3,4,6,7,9,10};
-    int ele=8;
+    int ele=9;
     int mid;
     int low=0,high=9,flag=0;
     
     while(low<high)
     {
          mid= (low+high)/2;
-        if(a[mid]<ele)
+        
+     
+        
+         if(a[mid]<=ele)
         low=mid+1;
+        
         else
          high=mid;
     }
