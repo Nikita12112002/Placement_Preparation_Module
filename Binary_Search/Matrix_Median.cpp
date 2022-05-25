a8283cc7df7e11df15d3a5af4555ba89a4a641b6
Nikita12112002 <guptaniki488@gmail.com>
2022-05-25 16:20:07 +0530

    commit

--- Binary_Search/Matrix_Median.cpp
+++ Binary_Search/Matrix_Median.cpp
@@ -4,3 +4,41 @@
 // SC:O(n*m)
 
 // optimal approach--> using binary search
+
+int func(vector<int>&A  ,int mid)
+{
+    int low=0 , high=A.size()-1;
+    
+    while(low<=high)
+    {
+        int md=(low+high)/2;
+        
+        if(A[md]<=mid)
+            low=md+1;
+        else
+            high=md-1;
+    }
+    return low;
+}
+int getMedian(vector<vector<int>> &matrix)
+{
+    // Write your code here.
+    
+    int low=1 , high=1e9;
+    int mid,count;
+    int n=matrix.size();
+    int m=matrix[0].size();
+    while(low<=high)
+    {
+        mid=(low+high)/2;
+        count=0;
+        for(int i=0 ;i<n;i++)
+            count=count+func(matrix[i],mid);
+        
+        if(count<=(n*m)/2)
+            low=mid+1;
+        else
+            high=mid-1;
+    }
+    return low;
+}
