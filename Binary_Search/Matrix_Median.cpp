23974b5b114bcb92d34c8983212ec07e6a68f123
Nikita12112002 <guptaniki488@gmail.com>
2022-05-25 08:04:32 +0530

    commit

---
+++ Binary_Search/Matrix_Median.cpp
@@ -0,0 +1,4 @@
+// brute force -- iterate every element and put it in a new data structure
+// sort the data structure and find the median
+// TC : n*m for putting all the ele in data structure and for sorting n*m logn(n*m)
+// SC:O(n*m)
