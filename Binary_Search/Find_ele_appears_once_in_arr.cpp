ac38263c21ace32406a45b3575ba755a5e931e0d
Nikita12112002 <guptaniki488@gmail.com>
2022-05-26 16:29:24 +0530

    commit

--- Binary_Search/Find_ele_appears_once_in_arr.cpp
+++ Binary_Search/Find_ele_appears_once_in_arr.cpp
@@ -14,3 +14,32 @@ public:
 };
 
 // optimal --using binary search -->TC:O(logn)
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+      //note--
+      //right half--> 1st instance-odd index
+      //               2nd instance--even index
+        
+       //left half--> 1st instance-->even index
+       //             2nd instance-->odd index
+        int low=0 ,high=nums.size()-2;
+        
+        while(low<=high)
+        {
+           int mid=(low+high)/2;
+            
+        // note- xor of even index with 1 always gives odd index
+        // and xor of odd index with 1 always give even index
+            
+            if(nums[mid]==nums[mid^1]) // signifies we are on the left half
+            // mid^1 is going to give previous even or next odd
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return nums[low];
+        
+    }
+};
