000ba819a7b9c37758034341ddd06f6f80a9891f
Nikita12112002 <guptaniki488@gmail.com>
2022-06-02 21:55:00 +0530

    commit

---
+++ String/Longest_common_prefix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    //TC:O(size Of first_string * no of strings)
+    string longestCommonPrefix(vector<string>& s) {
+        
+       string ans="";
+        for(int i=0 ;i<s[0].size();i++)
+        {
+            for(int j=1 ;j<s.size();j++)
+            {
+                if(s[0][i]!=s[j][i])
+                   return ans;
+            }
+          ans=ans+s[0][i];
+        }
+        return ans;           
+                   
+    }
+};
