17376515c520c7317f3df17dcdf5a71f2d253f21
Nikita12112002 <guptaniki488@gmail.com>
2022-06-02 22:19:52 +0530

    commit

---
+++ String/Implement_atoi_strstr.cpp
@@ -0,0 +1,5 @@
+// What is the difference between stoi and atoi?
+// First, atoi() converts C strings (null-terminated character arrays) to an integer,
+// while stoi() converts the C++ string to an integer. 
+//Second, the atoi() function will silently fail if the string is not convertible 
+//to an int , while the stoi() function will simply throw an exception.
