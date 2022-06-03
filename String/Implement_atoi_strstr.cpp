f45a1c2b4a731087f77a49e9a5eec3e47dacb2e1
Nikita12112002 <guptaniki488@gmail.com>
2022-06-03 09:47:33 +0530

    commit

--- String/Implement_atoi_strstr.cpp
+++ String/Implement_atoi_strstr.cpp
@@ -3,3 +3,12 @@
 // while stoi() converts the C++ string to an integer. 
 //Second, the atoi() function will silently fail if the string is not convertible 
 //to an int , while the stoi() function will simply throw an exception.
+
+// In the question we have to handle four cases-->
+
+// 1. discards all leading whitespaces
+// 2. sign of the number
+// 3. if overflow then simply return -2^31
+// 4. invalid input
+// 5. if fisrt sequence is not numeric then simply return 0
+// 6. if we have word after a numeric value then simply return integer part
