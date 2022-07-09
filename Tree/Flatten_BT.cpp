a3f3a811b17d8c642b8f619229c1b9b3548cf638
Nikita12112002 <guptaniki488@gmail.com>
2022-07-10 00:10:47 +0530

    commit

--- Tree/Flatten_BT.cpp
+++ Tree/Flatten_BT.cpp
@@ -1,57 +1,83 @@
-//Recursive
+// Recursive
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* prev=NULL;
-    void flatten(TreeNode* root) {
-        
-        if(root==NULL)
+    TreeNode *prev = NULL;
+    void flatten(TreeNode *root)
+    {
+
+        if (root == NULL)
             return;
-        
+
         flatten(root->right);
         flatten(root->left);
-        
-        root->right=prev;
-        root->left=NULL;
-        prev=root;
-        
+
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
     }
 };
 
 // iterative
 
-class Solution {
+class Solution
+{
 public:
-    TreeNode* prev=NULL;
-    void flatten(TreeNode* root) {
-        
-       stack<TreeNode*>st;
+    TreeNode *prev = NULL;
+    void flatten(TreeNode *root)
+    {
+
+        stack<TreeNode *> st;
         st.push(root);
-        
-        if(root==NULL)
+
+        if (root == NULL)
             return;
-        
-        while(!st.empty())
+
+        while (!st.empty())
         {
             auto node = st.top();
             st.pop();
-            
-            if(node->right)
+
+            if (node->right)
                 st.push(node->right);
-            
-            if(node->left)
+
+            if (node->left)
                 st.push(node->left);
-            
-            if(!st.empty())
-              node->right=st.top();
-            
-            node->left=NULL;
-                
+
+            if (!st.empty())
+                node->right = st.top();
+
+            node->left = NULL;
         }
     }
 };
 
 // SC:O(1)
 
+class Solution
+{
+public:
+    void flatten(TreeNode *root)
+    {
+        TreeNode *node = root;
+        while (node)
+        {
+            if (node->left)
+            {
+                TreeNode *pre = node->left;
 
+                while (pre->right)
+                {
+                    pre = pre->right;
+                }
 
+                pre->right = node->right;
+                node->right = node->left;
+                node->left = NULL;
+            }
+
+            node = node->right;
+        }
+    }
+};
