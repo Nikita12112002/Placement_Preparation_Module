148a1cf037ac3166dd8b5cc150cb39c302be8cdc
Nikita12112002 <guptaniki488@gmail.com>
2022-07-10 16:14:24 +0530

    commit

---
+++ Tree/Binary_Search_Tree/Search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        if(root==NULL)
+            return NULL;
+        
+        if(root->val == val)
+            return root;
+        
+        if(val<root->val)
+            return searchBST(root->left,val);
+        
+        else
+            return searchBST(root->right ,val);
+        
+    }
+};
