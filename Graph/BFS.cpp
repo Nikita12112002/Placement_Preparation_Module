51c9ea9380ca242aace6595593db333ebe4df1fa
Nikita12112002 <guptaniki488@gmail.com>
2022-07-13 14:20:16 +0530

    commit

---
+++ Graph/BFS.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void bfs(vector<int> adj[] , int n , int s)
+{
+    queue<int>q;
+    
+    bool visited[n];
+    
+    for(int i=0 ; i<n ;i++)
+    {
+        visited[i]=false;
+    }
+    
+    visited[s]=true;
+    q.push(s);
+    
+    while(q.empty()==false)
+    {
+       int u=q.front();
+        cout<<u<<" ";
+        q.pop();
+        
+        for(auto v:adj[u])
+        {
+            if(visited[v]==false)
+            {
+                visited[v]=true;
+                q.push(v);
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    
+    int n , e;
+    cin>>n>>e;
+    
+    vector<int> adj[n+1];
+    
+    for(int i=0 ; i<e ;i++)
+    {
+        int u,v;
+        cin>>u>>v;
+        
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+        
+        
+        cout<<"BFS TRAVERSAL IS :"<<endl;
+        bfs(adj,n,0); // source is given
+        
+    
+
+    return 0;
+}
