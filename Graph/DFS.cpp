cc374fe7846a57d25272fe56e97180996b834d37
Nikita12112002 <guptaniki488@gmail.com>
2022-07-13 18:09:47 +0530

    commit

---
+++ Graph/DFS.cpp
@@ -0,0 +1,44 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+void dfs(int node,bool visited[] ,vector<int>adj[])
+{
+    visited[node]=1;
+    
+    cout<<node<<" ";
+    
+    for(auto it:adj[node]){
+        if(!visited[it])
+        dfs(it,visited,adj);
+    }
+}
+void CheckDfs(int n,vector<int>adj[] ,int s)
+{
+   bool visited[n];
+   
+   for(int i=0 ;i<n;i++)
+   {
+       visited[i]=false;
+   }
+   
+   dfs(s,visited,adj);
+}
+
+int main()
+{
+   int n;
+   cin>>n;
+   int e;
+   cin>>e;
+   vector<int>arr[n+1];
+   for(int i=0;i<e;i++){
+       int u,v;
+       cin>>u>>v;
+       arr[u].push_back(v);
+       arr[v].push_back(u);
+   }
+  CheckDfs(n,arr,0);
+   
+    return 0;
+}
