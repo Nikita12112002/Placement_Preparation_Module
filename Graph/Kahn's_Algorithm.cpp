de314bcb5a06c53cb138a3ea4a6ca17147a73430
Nikita12112002 <guptaniki488@gmail.com>
2022-07-14 22:53:11 +0530

    commit

---
+++ Graph/Kahn's_Algorithm.cpp
@@ -0,0 +1,60 @@
+// also known as topological sort using bfs.
+// concept-> indegree.
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int n,e ;
+   cin>>n>>e;
+   
+   vector<int>adj[n+1];
+   
+   for(int i=0 ;i<e ;i++)
+   {
+       int u,v;
+       cin>>u>>v;
+       adj[u].push_back(v);  // for directed graph.
+   }
+   
+   vector<int>indegree(n+1);
+   
+   for(int i=0 ;i<n;i++)
+   {
+       for(auto it:adj[i])
+       {
+           indegree[it]++;
+       }
+   }
+   
+   queue<int>q;
+   
+   for(int i=0 ;i<n;i++)
+   {
+       if(indegree[i]==0)
+        q.push(i);
+   }
+   
+   vector<int>ans;
+   
+   while(!q.empty())
+   {
+       auto node = q.front();
+       q.pop();
+       
+       ans.push_back(node);
+       
+       for(auto it :adj[node])
+       {
+           indegree[it]--;
+           if(indegree[it]==0)
+            q.push(it);
+       }
+   }
+   
+   for(int i=0 ;i<ans.size() ;i++)
+   {
+       cout<<ans[i]<<" ";
+   }
+    return 0;
+}
