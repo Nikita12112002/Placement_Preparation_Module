6f6f977cd9d3e72a45ed10ba8fdf62876ee9c821
Nikita12112002 <guptaniki488@gmail.com>
2022-07-27 23:14:19 +0530

    commit

---
+++ Dynamic_Programming/Total_unique_Paths_in_grid/Tabulation.cpp
@@ -0,0 +1,31 @@
+int countWaysUtil(int m, int n, vector<vector<int> > &dp) {
+  for(int i=0; i<m ;i++)
+  {
+      for(int j=0; j<n; j++)
+      {
+          if(i==0 && j==0)
+          {
+              dp[i][j]=1;
+              continue;
+          }
+          
+          int up=0;
+          int left = 0;
+          
+          if(i>0) 
+            up = dp[i-1][j];
+          if(j>0)
+            left = dp[i][j-1];
+            
+          dp[i][j] = up+left;
+      }
+  }
+  
+  return dp[m-1][n-1];
+}
+
+int uniquePaths(int m, int n) {
+	// Write your code here.
+      vector<vector<int> > dp(m,vector<int>(n,-1));
+    return countWaysUtil(m,n,dp);
+}
