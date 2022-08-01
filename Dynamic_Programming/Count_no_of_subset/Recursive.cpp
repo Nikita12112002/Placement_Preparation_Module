b869814ee3ff3365058e2a912577f909b0ef641b
Nikita12112002 <guptaniki488@gmail.com>
2022-08-01 15:14:22 +0530

    commit

---
+++ Dynamic_Programming/Count_no_of_subset/Recursive.cpp
@@ -0,0 +1,23 @@
+int dfs(vector<int>&num , int tar , int i)
+{
+    if(tar==0)
+        return 1;
+    if(i==0)
+    {
+        if(num[i]==tar)
+            return 1;
+        else
+            return 0;
+    }
+    int notpick = dfs(num,tar,i-1);
+    int pick =0;
+    if(num[i]<=tar)
+        pick = dfs(num ,tar-num[i],i-1);
+    return pick+notpick;
+}
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+    int n = num.size();
+    return dfs(num,tar,n-1);
+}
