d7a18b0b9679f83dce6c14c945126de811089aa3
Nikita12112002 <guptaniki488@gmail.com>
2022-08-01 15:42:03 +0530

    commit

---
+++ Dynamic_Programming/Count_no_of_subset/Memoization.cpp
@@ -0,0 +1,26 @@
+int dfs(vector<int>&num , int tar , int i , vector<vector<int>>&dp)
+{
+    if(tar==0)
+        return 1;
+    if(i==0)
+    {
+        if(num[i]==tar)
+            return 1;
+        else
+            return 0;
+    }
+    if(dp[i][tar]!=-1)
+        return dp[i][tar];
+    int notpick = dfs(num,tar,i-1,dp);
+    int pick =0;
+    if(num[i]<=tar)
+        pick = dfs(num ,tar-num[i],i-1,dp);
+    return dp[i][tar]=pick+notpick;
+}
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here.
+    int n = num.size();
+    vector<vector<int>>dp(n,vector<int>(tar+1,-1));
+    return dfs(num,tar,n-1,dp);
+}
