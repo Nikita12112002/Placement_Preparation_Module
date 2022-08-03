d51d6eee894f206878f408c589348190646375f9
Nikita12112002 <guptaniki488@gmail.com>
2022-08-03 19:21:41 +0530

    commit

---
+++ Dynamic_Programming/Longest_Common_Substring/Recusrive.cpp
@@ -0,0 +1,26 @@
+int solve(string &str1, string &str2, int i,int j,int &count)
+{
+    if(i<0||j<0) return 0;
+    
+//     if(dp[i][j]!=-1) return dp[i][j];
+    
+    int take=0;
+    if(str1[i]==str2[j])
+    {
+        take=1+solve(str1,str2,i-1,j-1,count);
+    }
+    int a=solve(str1,str2,i-1,j,count);
+   int b=solve(str1,str2,i,j-1,count);
+    count=max(count,take);
+    
+    return take;
+}
+int lcs(string &str1, string &str2){
+    int n=str1.size();
+    int m=str2.size();
+    int count=0;
+//     vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+      solve(str1,str2,n-1,m-1,count);
+    
+    return count;
+}
