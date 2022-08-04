be919d12d3f46f7fb926bb9c46ae19cc81752da6
Nikita12112002 <guptaniki488@gmail.com>
2022-08-04 16:58:11 +0530

    commit

---
+++ Dynamic_Programming/Edit_Distance/Tabulation.cpp
@@ -0,0 +1,40 @@
+public:
+    int dfs(string &word1 , string &word2 , int i , int j , vector<vector<int>>&dp)
+    {
+        if(i<0)
+            return j+1;
+        if(j<0)
+            return i+1;
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        if(word1[i]==word2[j])
+            return dp[i][j]=dfs(word1 , word2 , i-1 , j-1,dp);
+        
+        return dp[i][j] = 1+min(dfs(word1,word2,i,j-1,dp),min(dfs(word1,word2,i-1,j,dp) ,dfs(word1,word2,i-1,j-1,dp)));
+    }
+    int minDistance(string word1, string word2) {
+        
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        
+        for(int i=0 ; i<=n ;i++)
+        dp[i][0]=i;
+        
+        for(int i=0 ;i<=m ;i++)
+            dp[0][i]=i;
+        
+        for(int i=1 ; i<=n ;i++)
+        {
+            for(int j=1 ; j<=m ;j++)
+            {
+                if(word1[i-1]==word2[j-1])
+                 dp[i][j]=dp[i-1][j-1];
+                else
+                    
+                  dp[i][j] = 1+min(dp[i][j-1] , min(  dp[i-1][j] ,dp[i-1][j-1]) );
+            }
+        }
+        return dp[n][m];
+    }
+};
