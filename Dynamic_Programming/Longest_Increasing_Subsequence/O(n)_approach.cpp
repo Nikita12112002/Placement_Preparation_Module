23bc43e1fbd7893440c34105d15aeeaa52255ce9
Nikita12112002 <guptaniki488@gmail.com>
2022-08-08 21:47:26 +0530

    commit

---
+++ Dynamic_Programming/Longest_Increasing_Subsequence/O(n)_approach.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        
+        int ans = 1;
+        int n = nums.size();
+        vector<int> dp(n, 1);
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < i; j++) 
+            {
+                if(nums[i] > nums[j]) 
+				    dp[i] = max(dp[i], dp[j] + 1);
+                  ans = max(ans, dp[i]);
+            }
+        }
+        return ans;
+    }
+        
+    
+};
